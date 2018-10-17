a7270ddca64f4ca8555962f58e02731a68733ddd
Daan de Bruijn <ddebruijn9@gmail.com>
2018-10-17 17:40:20 +0200

    Hulp nodig! (Gratis Devpoints!!)
    Hoi allemaal. Dit inheritance enzo is allemaal nog redelijk nieuw voor mij en ik heb er best nog wat moeite mee. Ik wil graag mijn enemies laten bewegen met de Move() functie die in mijn Enemy Script staan (De enemies inheriten dit script). In de GameManager roep ik deze functie aan (En een functie die de snelheid bepaald). Het werkt alleen niet. Ik krijg deze error in de console:
    NullReferenceException: Object reference not set to an instance of an object
    GameManager.Update () (at Assets/Scripts/GameManager.cs:21)
    
    Plox help! (Eerste die mij kan helpen krijgt gratis like!!!!!!).

---
+++ DaandB/Week 6/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public Croc croc;
+	public Log log;
+	public Car car;
+	public Snake snake;
+
+	void Start(){
+		croc.SetSpeed();
+		log.SetSpeed();
+		car.SetSpeed();
+		snake.SetSpeed();
+	}
+
+	void Update(){	
+		croc.Move();	
+		log.Move();
+		car.Move();
+		snake.Move();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player"){
+			SceneManager.LoadScene(Random.Range(1, 3));
+		} else return;
+	}
+}
