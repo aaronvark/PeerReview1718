36e5ad75609334334810bce76371935edcf8c5d8
Daan de Bruijn <ddebruijn9@gmail.com>
2018-10-18 19:54:42 +0200

    Poeh! Eindelijk gelukt!
    Oke ik heb dit inheritance gedoe eindelijk door! Beoordeel mijn code voor gratis devpoints. (Het kan zijn dat de comments nog outdated zijn maar daar zit ik aan te werken ;))
    
    P.S. sorry voor de lange lijst files. Je mag wat mij betreft ook gewoon accepten/changes requesten voor die oh zo zoete devpoints.

---
+++ DaandB/Week 7/Car.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : Enemy {
+	
+	void Awake(){
+		StartCoroutine(CarRot());
+	}
+
+	public override void Move(){ base.Move(); }
+
+	private IEnumerator CarRot(){
+		yield return new WaitForSeconds(0.25f);
+		transform.Rotate(0f, 0f, Random.Range(-15f, 15f));
+	}
+
+	private void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player"){
+			PlayerDeath.DeathBy(this);
+		} else if (other.tag == "Car") transform.Rotate(0f, 0f, Random.Range(-30, 30));
+	}
+} 
---
+++ DaandB/Week 7/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public Rigidbody2D rigBod;
+
+	public float minSpeed = 1;
+	public float maxSpeed = 5;
+
+	public float speed;
+
+	// SetSpeed sets the speed variable and assigns the object's rigidbody to the rigBod variable.
+	public void SetSpeed () {
+		rigBod = GetComponent<Rigidbody2D>();
+
+		speed = Random.Range(minSpeed, maxSpeed);
+	}
+	
+	// Move moves the object's rigidbody.
+	public virtual void Move () {
+		Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+
+		rigBod.MovePosition(rigBod.position + forward * Time.fixedDeltaTime * speed);
+		
+	}
+}
