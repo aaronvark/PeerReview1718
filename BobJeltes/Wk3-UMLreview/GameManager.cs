76516699afd04cadef68a67d48efe4c9e3e89bcc
Bob <bobjeltes@hotmail.com>
2018-10-21 17:23:31 +0200

    Week 3 - UML Review Pepijn
    - UML toegepast in code
    - UML herzien in code
    - UML opnieuw ingedeeld om circular dependencies te vermijden, en de GameManager iets meer verantwoordelijkheid te geven

---
+++ BobJeltes/Wk3-UMLreview/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pepijn {
+
+    public class GameManager : MonoBehaviour {
+        public TetrisChecker TetrisChecker { get; set; }
+        public Block Block { get; set; }
+
+        public GameObject RowPrefab { get; set; }
+        public GameObject Roof { get; set; }
+        public GameObject RightWall { get; set; }
+        public GameObject LeftWall { get; set; }
+        public GameObject BackWall { get; set; }
+        public GameObject Ground { get; set; }
+        public GameObject LatestRow { get; set; }
+
+        public bool BlockIsFallin { get; set; }
+
+
+        private void Start() {
+            
+        }
+
+
+        private void Update() {
+
+        }
+
+
+        public void CreateBlock() {
+            
+        }
+
+
+        public void Tetris() {
+
+        }
+
+
+    }
+}
