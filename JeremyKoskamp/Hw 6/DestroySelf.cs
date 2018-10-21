eadbe682da443690e4a58cd50e38c9af872791ff
Unknown <jeremy.koskamp@live.nl>
2018-10-22 00:13:37 +0200

    [+] My Life

---
+++ JeremyKoskamp/Hw 6/DestroySelf.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour {
+
+    private ParticleSystem pr;
+
+    private void Start() {
+        pr = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if ( other.tag != ("gun") ) {
+            if ( other.tag != ("Player") ) { 
+                pr.transform.parent = null;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
