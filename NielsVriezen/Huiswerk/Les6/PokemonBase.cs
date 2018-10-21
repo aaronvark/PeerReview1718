ef71dfe7019e71cd8a4e5354f8cb78ad23374d7f
NVriezen <36536400+NVriezen@users.noreply.github.com>
2018-10-21 15:04:34 +0200

    Huiswerk Week 6

---
+++ NielsVriezen/Huiswerk/Les6/PokemonBase.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PokemonBase : MonoBehaviour {
+
+	public int DexNum { get; set; }
+	public int DexNumJohto { get; set; }
+	public int Health { get; protected set; }
+	public int CurrentHealth { get; set; }
+	public string Type1 { get; protected set; }
+	public string Type2 { get; protected set; }
+	public int Attack { get; protected set; }
+	public int Defense { get; protected set; }
+	public int Speed { get; protected set; }
+	public int Level { get; set; }
+	public int Exp { get; set; }
+	public bool Gender { get; protected set; }
+	public bool fainted { get; set; }
+	public bool Shiny{ get; protected set; }
+	public string Name { get; set; }
+
+	private static float shinyRate = 8192;
+
+	//PokemonBase (){
+	//	return this;
+	//}
+
+	//Debugging
+	public static PokemonBase RandomPokemon ( int dexNum ) {
+		GameObject temp = new GameObject ();
+		Debug.Log ( "GameObject is set" + temp.name );
+		//Pokemon pokemonData = PokemonDataManager.GetPokemon ();
+		PokemonDataManager data = temp.AddComponent<PokemonDataManager> ();
+		Debug.Log ( "New data Manager created" );
+		Pokemon pokemonData = data.GetPokemon ( dexNum );
+		Debug.Log ( "Got our new pokemon" );
+		Debug.Log ( pokemonData.Name );
+		PokemonBase poke = temp.AddComponent<PokemonBase> ();
+		Debug.Log ( "Pokemon Added to temp GameObject" );
+		//this = temp.AddComponent<PokemonBase> ();
+		//PokemonBase poke = new PokemonBase ();
+		poke.DexNum = pokemonData.No;
+		poke.DexNumJohto = pokemonData.JohtoNo;
+		poke.Type1 = pokemonData.Type1;
+		poke.Type2 = pokemonData.Type2;
+		poke.Health = pokemonData.BaseStats.HP;
+		poke.Attack = pokemonData.BaseStats.Attack;
+		poke.Defense = pokemonData.BaseStats.Defense;
+		poke.Speed = pokemonData.BaseStats.Speed;
+		poke.Name = pokemonData.Name;
+		if ( Random.Range ( 0.0f, 1.0f ) == 0.5f ) {
+			poke.Gender = true;
+		} else {
+			poke.Gender = false;
+		}
+				
+		if ( Random.Range ( 0, shinyRate ) == 0 ) {
+			poke.Shiny = true;
+		} else {
+			poke.Shiny = false;
+		}
+		return poke;
+	}
+
+
+	PokemonBase NewPokemon () {
+		PokemonBase poke = new PokemonBase ();
+		poke.Level = 5;
+		poke.Exp = 0;
+		return poke;
+	}
+
+
+	PokemonBase NewPokemon ( int dexNum ) {
+		PokemonBase poke = new PokemonBase ();
+		poke.Level = 5;
+		poke.Exp = 0;
+		return poke;
+	}
+
+
+	void SetStats () {
+		//Get everything from the JSON via DexNum;
+	}
+}
