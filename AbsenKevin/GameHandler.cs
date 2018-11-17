a3b1dc6a7e8b6862bd7fcd1f5e2f95f7ab8dacf9
EvocateHimself <kevin.absen@hotmail.com>
2018-11-17 16:57:42 +0100

    Tools HW1 - JSON research
    Simpel scriptje dat een JSON bestand kan lezen en schrijven en de input gebruikt voor een UI text met position, color en string.

---
+++ AbsenKevin/GameHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHandler : MonoBehaviour {
+
+    [SerializeField]
+    private Text m_text;
+
+    // Use this for initialization
+    private void Start () {
+        Debug.Log("Gamehandler.start");
+
+        /*
+        // Create new user data variables and save to a json script
+        UserData userData = new UserData();
+        userData.position = new Vector3(280, 160, 0);
+        userData.color = new Color(255,0,0);
+        userData.text = "This is a text created by a json script.";
+
+        string json = JsonUtility.ToJson(userData);
+        Debug.Log(json);
+
+        File.WriteAllText(Application.dataPath + "/saveFile.json", json);
+        */
+
+        // Read and load the user data variables from json script
+        string json = File.ReadAllText(Application.dataPath + "/saveFile.json");
+        UserData loadedUserData = JsonUtility.FromJson<UserData>(json);
+        m_text.transform.position = loadedUserData.position;
+        m_text.color = loadedUserData.color;
+        m_text.text = loadedUserData.text;
+    }
+}
