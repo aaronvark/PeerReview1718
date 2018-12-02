5d4126ab8e3bbcfc1c8b52fb6d50b896d02a6e7a
Unknown <jeremy.koskamp@live.nl>
2018-12-02 15:10:20 +0100

    [/] Mapp Layout [+] Circle FOrmation Pattern

---
+++ JeremyKoskamp/Blok 2/Week 3/CircleFormation.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleFormation : MonoBehaviour {
+
+    public Transform markerPrefab;
+    [Range(1, 100)]
+    public int pointCount;
+    public float radius;
+
+    public List<Transform> markers;
+
+
+    public void Start()
+    {
+        markers = new List<Transform>();
+        InitMarkers();
+    }
+
+    public void Update()
+    {
+        if(pointCount != markers.Count)
+        {
+            InitMarkers();
+        }
+
+        ////// the core bit ///////
+        Quaternion quaternion = Quaternion.AngleAxis(360f / (float)(pointCount), transform.up);
+        Vector3 vec3 = transform.forward * radius;
+        for(int index = 0; index < pointCount; ++index)
+        {
+            markers[index].position = transform.position + vec3;
+            // update for the next one
+            vec3 = quaternion * vec3;
+        }
+        ////// end of the core bit ///////
+    }
+
+    private void InitMarkers()
+    {
+        if(pointCount > markers.Count)
+        {
+            for(int i = markers.Count; i < pointCount; i++)
+            {
+                // doesn't matter, we're updating the positions later
+                markers.Add(Instantiate(markerPrefab, Vector3.zero, Quaternion.identity) as Transform);
+                markers[i].transform.parent = gameObject.transform;
+            }
+        }
+        if(pointCount < markers.Count)
+        {
+            while(pointCount < markers.Count)
+            {
+                Destroy(markers[0].gameObject);
+                markers.RemoveAt(0); // dont miss this line out!!
+            }
+        }
+    }
+}
+
